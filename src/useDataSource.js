8920ada4dc2ae7cf56f0ec83b7f45bbf059ec185
MichaelMburu <ichangaimichael@yahoo.com>
2022-07-27 16:07:48 +0300

    Custom Hooks - Create useResource custom hook that takes in an id and appends to the resource URL in any component

---
+++ src/useDataSource.js
@@ -0,0 +1,16 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
+export const useDataSource = getResourceFunction => {
+
+    const [resource, setResource] = useState(null);
+
+    useEffect(() => {
+        (async () =>{
+            const result = await getResourceFunction()
+            setResource(result)
+        })();
+    }, [getResourceFunction])
+
+    return resource;
+}
